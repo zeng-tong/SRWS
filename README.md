bc85f2a4d8eb781510b4345b4ef6cdb07b35c968
zengtong <unixsudo@outlook.com>
2018-06-17 15:25:30 +0800

    完成echo 服务器功能，客户端发送消息被正常回射。

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Simple-Web-Server ![ci](https://travis-ci.com/zeng-tong/SRWS.svg?branch=master)
+# Simple-Web-Server 
+![ci](https://travis-ci.com/zeng-tong/SRWS.svg?branch=master)
+[![codecov](https://codecov.io/gh/zeng-tong/SRWS/branch/master/graph/badge.svg)](https://codecov.io/gh/zeng-tong/SRWS)
+
 This is a skeleton project of Simple CGI server based on Reactor.
--- test/processPoolTest.cpp
+++ test/processPoolTest.cpp
@@ -4,6 +4,9 @@
 #include "tcpServer.h"
 
 
+static void sigalrmHandler(int sig) {
+    kill(getpid(), SIGINT);
+}
 
 TEST_CASE("test echoClient should send data and receive the same") {
     pid_t  pid = fork();
@@ -18,5 +21,33 @@ TEST_CASE("test echoClient should send data and receive the same") {
         auto output = echoClient.getMessage();
         echoClient.disConnect();
         REQUIRE(input == output);
+    }
+}
+
+TEST_CASE("test ProcessPool, echoClient should receive the same data of its sent") {
+    const int LISTEN_SUCCESS = 0;
+    TcpServer tcpServer;
+    EchoClient echoClient;
+    const char *ip = "127.0.0.1";
+    const short port = 13233;
+
+    if (tcpServer.startListen(ip, port) == LISTEN_SUCCESS) {
+        auto pool = ProcessPool<EchoServer>::getInstance(tcpServer.getSockFd(), 8);
+        pid_t  pid = fork();
+        if (pid > 0) {
+            sleep(2); // assure pool run first
+            close(tcpServer.getSockFd());
+            echoClient.startConnect(ip, port);
+            std::string input = "HELLO";
+            echoClient.sendMessage(input.c_str());
+            auto output = echoClient.getMessage();
+            echoClient.disConnect();
+            REQUIRE(input == output);
+        } else {
+            alarm(10);
+            signal(SIGALRM, sigalrmHandler);
+            pool->run();
+        }
+        delete pool;
     }
 }
